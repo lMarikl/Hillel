394d87fac1c94de31ade8824542ebb20a0a0ce94
Marik <mixa.marik@gmail.com>
2018-07-05 12:52:33 +0300

    IntArrayList<E>

--- src/homework7/IntArrayList.java
+++ src/homework7/IntArrayList.java
@@ -86,9 +86,11 @@ public class IntArrayList<E> implements IntList<E>{
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < values.length; i++) {
-            sb.append(values[i]);
-            if (i != values.length-1){
-                sb.append(", ");
+            if (values[i] != null){
+                sb.append(values[i]);
+                if (i != values.length-1){
+                    sb.append(", ");
+                }
             }
         }
         return "[" + sb.toString() + "]";
---
+++ src/homework7/Main.java
@@ -0,0 +1,11 @@
+package homework7;
+
+public class Main {
+    public static void main(String[] args) {
+        IntArrayList<String> list = new IntArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        System.out.println(list);
+    }
+}
